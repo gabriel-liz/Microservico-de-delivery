3983f2b412752cba8624cb1e633e4636e258a974
gabriel-liz <gabriel.liz@outlook.com.br>
2025-09-19 16:26:34 -0300

    Implementado alterações em filhos de um Aggregate

--- Delivery-Tracking/src/main/java/com/delivery/delivery/tracking/domain/model/Delivery.java
+++ Delivery-Tracking/src/main/java/com/delivery/delivery/tracking/domain/model/Delivery.java
@@ -49,8 +49,37 @@ public class Delivery {
         return delivery;
     }
 
+    public UUID addItem(String name, int quantity){
+        Item item = Item.brandNew(name, quantity);
+        items.add(item);
+        calculateTotalItems();
+        return item.getId();
+    }
+
+    public void removeItem(UUID itemId){
+        items.removeIf(item -> item.getId().equals(itemId));
+        calculateTotalItems();
+    }
+
+    public void changeItemQuantity(UUID itemId, int quantity){
+        Item item = getItems().stream().filter(i -> i.getId().equals(itemId))
+                .findFirst().orElseThrow();
+        item.setQuantity(quantity);
+        calculateTotalItems();
+    }
+
+    public void removeItems(){
+        items.clear();
+        calculateTotalItems();
+    }
+
     //Esse get serve para que quem fizer o get nao possa alterar a lista de itens
     public List<Item> getItems() {
         return Collections.unmodifiableList(this.items);
+    }
+
+    private void calculateTotalItems(){
+        int totalItems = getItems().stream().mapToInt(Item::getQuantity).sum();
+        setTotalItems(totalItems);
     }
 }
