367575bad7e89237af7aed82ddab098e5c5d791b
gabriel-liz <gabriel.liz@outlook.com.br>
2025-09-19 15:46:03 -0300

    Implementado a criação das entidades

---
+++ Delivery-Tracking/src/main/java/com/delivery/delivery/tracking/domain/model/ContactPoint.java
@@ -0,0 +1,14 @@
+package com.delivery.delivery.tracking.domain.model;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class ContactPoint {
+
+    private String zipCode;
+    private String street;
+    private String numer;
+    private String complement;
+    private String name;
+    private String phone;
+}
---
+++ Delivery-Tracking/src/main/java/com/delivery/delivery/tracking/domain/model/Item.java
@@ -0,0 +1,13 @@
+package com.delivery.delivery.tracking.domain.model;
+
+import lombok.EqualsAndHashCode;
+
+import java.util.UUID;
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Item {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+    private String name;
+    private Integer quantity;
+}
